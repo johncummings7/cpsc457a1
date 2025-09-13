c6a12d4db4b3e4d757f82432f618416022e91ce2
johncummings7 <jp7c@icloud.com>
2025-09-13 13:11:07 -0600

    entry point in a1p1

--- a1p1.c
+++ a1p1.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
 int main(){
-	printf("hello world\n");
-	return 0;
+	
+    int matrix[100][1000];
+
+    // Read and store matrix from input file
+    for (int row = 0; row < 100; row++) {
+        for (int col = 0; col < 1000; col++) {
+            // Ensures valid input along the way
+            if (scanf("%d", &matrix[row][col]) != 1) {
+                fprintf(stderr, "Error: bad input at coordinate [%d,%d]\n", row, col);
+                return 1;
+            }
+        }
+    }
+    // test
+    printf("In 'test1' there should be a 1 at coordinate 36, 309. Value read at row 36 col 309: %d\n", matrix[36][309]);
+
 }
