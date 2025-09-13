491363248a5a20f515cbf33cf818c025d4617aba
johncummings7 <jp7c@icloud.com>
2025-09-13 14:27:35 -0600

    Testing process creation. Program successfully forks 100 children and exits all child processes safely (i think)

--- a1p1.c
+++ a1p1.c
@@ -25,9 +25,31 @@ int main(){
         }
     }
 
-    // for (int row = 0; row < 100; row++){
-    //     pid_t pid = fork();
-    // }
+    fflush(NULL); // flushing io streams
+
+    // Testing process creation: forking 100 children
+    for (int row = 0; row < 100; row++){
+        pid_t fr = fork();
+
+        if (fr < 0) { // Error occurred 
+            fprintf(stderr, "Fork FAILED at row %d\n", row);
+            exit (-1);
+        }
+        if (fr == 0) { // Child process
+            printf("Child %d (PID %d): started\n", row, getpid());
+            exit(0);
+        } else { // Parent process
+            row_pid[row] = fr;
+            printf("Parent: forked child %d with PID %d\n", row, fr);
+        }
+    }
+
+    // Making sure processes exit
+    int status;
+    for (int i = 0; i < 100; i++) {
+        pid_t finished_pid = wait(&status);
+        printf("Parent: child with PID %d exited with status %d\n", finished_pid, WEXITSTATUS(status));
+    }
 
      
     return 0;
